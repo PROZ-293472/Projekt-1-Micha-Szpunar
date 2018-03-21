cfbd941e4ad85749b0927caa744efc1067f14b10
kalaflorian <37100483+kalaflorian@users.noreply.github.com>
2018-03-21 21:40:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,25 +9,54 @@ using namespace std;
 int main()
 {
     int choice;
-    int x1,y1,z1;
-    cin>>x1>>y1>>z1;
-    Tensor t1(x1,y1,z1),t2,t3;
+    int x,y,z;
+    Tensor t1,t2,t3;
     for(int i=0;i<10;i++){
-        cout<<"menu/n"<<endl;
+        cout<<"menu:"<<endl;
+        cout<<"1. podaj wymiary t1"<<endl;
+        cout<<"2. podaj wymiary t2"<<endl;
+        cout<<"3. podaj wartosci t1(>>t1)"<<endl;
+        cout<<"4. podaj wartosci t2(>>t2)"<<endl;
+        cout<<"5. drukuj t1 (<<t1)"<<endl;
+        cout<<"6. drukuj t2 (<<t2)"<<endl;
+        cout<<"7. drukuj t3 (<<t3)"<<endl;
+        cout<<"8. t3=t1+t2"<<endl;
+        cout<<"9. t1+=t2"<<endl;
+        cout<<"10. t3=t1-t2"<<endl;
+
+
+
         cin>>choice;
         switch(choice){
         case 1:
-            t2=t1;
-            cout<<t2;
+            cin>>x>>y>>z;
+            t1=Tensor(x,y,z);
             break;
-        /*case 2:
-            t1+=t2;
+        case 2:
+            cin>>x>>y>>z;
+            t2=Tensor(x,y,z);
             break;
         case 3:
-            t3=t1+t2;
-            break;*/
-        case 2:
+            cin>>t1;
+            break;
+        case 4:
+            cin>>t2;
+            break;
+        case 5:
             cout<<t1;
+            break;
+        case 6:
+            cout<<t2;
+            break;
+        case 7:
+            cout<<t3;
+            break;
+        case 8:
+            t3=t1+t2;
+            cout<<t3;
+            break;
+        case 9:
+            t1+=t2;
             break;
         }
 
